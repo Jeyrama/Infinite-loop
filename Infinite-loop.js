18b9a361325c31250419c3e0fa3cde819c6689b7
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-03-05 16:25:24 -0800

    Create Infinite-loop.js

---
+++ Infinite-loop.js
@@ -0,0 +1,25 @@
+
+// Fix this infinite loop
+
+function createArray(number) {
+  var newArray = [];
+
+  for(var counter = 1; counter <= number;) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
+
+
+// Solution
+
+function createArray(number) {
+  var newArray = [];
+
+  for(var counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
